1fd567d971959856e5918586e73bf0f24d098fe2
dksadasjkl <ckdgus6038@naver.com>
2024-04-05 11:29:56 +0900

    Feat : 해당 상품 페이지 전체 좋아수 개수 조회

---
+++ src/main/java/com/project/pet/service/ProductFavoriteService.java
@@ -0,0 +1,18 @@
+package com.project.pet.service;
+
+import com.project.pet.dto.product.response.ProductFavoriteResponseDto;
+import com.project.pet.entity.Product;
+import com.project.pet.repository.ProductFavoriteMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductFavoriteService {
+
+    @Autowired
+    private ProductFavoriteMapper productFavoriteMapper;
+
+    public ProductFavoriteResponseDto getProductFavoritesCount (int productId) {
+        return productFavoriteMapper.getProductFavoritesCount(productId).toProductFavoriteResponseDto();
+    }
+}
