3110af41bdcb3a126536015a81360f2a237e16d6
kwonkb98 <kwonkb98@gmail.com>
2024-04-05 10:33:02 +0900

    feat: 공지사항 단건 조회 기능추가

--- src/main/java/com/project/pet/service/admin/AdoptationBoardAdminService.java
+++ src/main/java/com/project/pet/service/admin/AdoptationBoardAdminService.java
@@ -3,8 +3,10 @@ package com.project.pet.service.admin;
 import com.project.pet.dto.adoptation.request.PostAdoptationBoardAdminReqDto;
 import com.project.pet.dto.adoptation.request.PostAdoptationBoardReqDto;
 import com.project.pet.dto.adoptation.request.UpdateAdoptationBoardReqDto;
+import com.project.pet.dto.adoptation.response.GetAdoptationBoardAdminRespDto;
 import com.project.pet.dto.adoptation.response.GetAdoptationBoardRespDto;
 import com.project.pet.entity.AdoptationBoard;
+import com.project.pet.entity.AdoptationBoardAdmin;
 import com.project.pet.repository.AdoptationBoardMapper;
 import com.project.pet.repository.admin.AdoptationBoardAdminMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,6 +24,13 @@ public class AdoptationBoardAdminService {
     //게시판 작성
     public void postAdoptationBoardAdmin(PostAdoptationBoardAdminReqDto postAdoptationBoardAdminReqDto) {
         adoptationBoardAdminMapper.postAdoptationBoardAdmin(postAdoptationBoardAdminReqDto.toEntity());
+    }
+
+    //게시판 단건 조회
+    public GetAdoptationBoardAdminRespDto getAdoptationBoardAdmin(int noticeId) {
+        AdoptationBoardAdmin adoptationBoardAdmin = adoptationBoardAdminMapper.getAdoptationBoardAdminByNoticeId(noticeId);
+
+        return adoptationBoardAdmin.toGetAdoptationBoardAdminRespDto();
     }
 
 
