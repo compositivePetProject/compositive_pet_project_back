5e29500e9d611f3288d14b69cbd132e1eb9ec8d6
kwonkb98 <kwonkb98@gmail.com>
2024-04-09 17:29:24 +0900

    feat: 해당 id로 좋아요 추가, 취소 기능 추가

--- src/main/java/com/project/pet/controller/AdoptationBoardFavoriteController.java
+++ src/main/java/com/project/pet/controller/AdoptationBoardFavoriteController.java
@@ -14,18 +14,23 @@ public class AdoptationBoardFavoriteController {
     @Autowired
     private AdoptationBoardFavoriteService adoptationBoardFavoriteService;
 
-
+    //해당 userId로 좋아요 취소
     @DeleteMapping("/favorite/{userId}")
     public ResponseEntity<?> deleteAdoptationBoardFavorite(@PathVariable int userId) {
         adoptationBoardFavoriteService.deleteAdoptationBoardFavorite(userId);
         return ResponseEntity.ok("좋아요 취소 완료");
     }
 
+
+    //해당 userId로 게시물에 좋아요 등록
     @PostMapping("/favorite")
     public ResponseEntity<?> saveAdoptationBoardFavorite(@RequestBody PostAdoptationBoardFavoriteReqDto postAdoptationBoardFavoriteReqDto) {
         adoptationBoardFavoriteService.saveAdoptationBoardFavorite(postAdoptationBoardFavoriteReqDto);
         return ResponseEntity.created(null).body(true);
     }
+
+
+
 
 
 }
