98ff93732100663f310f8651a91da7c136311d0f
kwonkb98 <kwonkb98@gmail.com>
2024-04-11 17:20:25 +0900

    feat: 동물 카테고리가 개인  리스트 다건 조회 기능 추가

--- src/main/java/com/project/pet/repository/AdoptationBoardMapper.java
+++ src/main/java/com/project/pet/repository/AdoptationBoardMapper.java
@@ -15,6 +15,8 @@ public interface AdoptationBoardMapper {
     //전체 게시글 조회
     public List<AdoptationBoard> getAdoptationBoards();
 
+    public List<AdoptationBoard> getAdoptationBoardsDog();
+
     //해당 유저id가 좋아요를 누른 게시글 리스트 조회
     public List<AdoptationBoard> getLikedAdoptationBoardByUserId(int userId);
 
