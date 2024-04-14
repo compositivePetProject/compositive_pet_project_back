b805152c1c4b0c3e94a92b51461b0ecb29f6d1fc
dksadasjkl <ckdgus6038@naver.com>
2024-04-15 00:51:24 +0900

    Style : 코드 수정

--- src/main/java/com/project/pet/repository/ProductMapper.java
+++ src/main/java/com/project/pet/repository/ProductMapper.java
@@ -2,12 +2,13 @@ package com.project.pet.repository;
 
 import com.project.pet.entity.product.Product;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
 @Mapper
 public interface ProductMapper {
-    public Product getProduct(int productId);
+    public Product getProduct(@Param("productId") int productId);
 
     public List<Product> getProducts();
 }
